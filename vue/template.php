ac834f78f100be38b02f1d8a2eb62c0c837229f9
wajdii <wajdibenhelal52@live.fr>
2018-07-20 19:31:26 +0100

    hellodddd

--- vue/template.php
+++ vue/template.php
@@ -1,6 +1,7 @@
-
+<html>
 <head>
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
     <title>Ajouter un dossier </title>
     <link rel="stylesheet" href="../Css/style.css" type="text/css" />
 </head>
+</html>
